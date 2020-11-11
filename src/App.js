ede41fddc1e712d366ab6b5d26e94e593067bc2b
daseulsong <vates0328@gmail.com>
2020-11-12 00:23:48 +0900

    add: CreateContext & useContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 
-function App() {
+// 0. AppContext 생성
+const AppContext = createContext();
+
+const App = () => {
+  const user = {
+    name: "daseul",
+    isAdmin: true,
+  };
+
   return (
-    <div className="App">
-      <h1>hello world!</h1>
+    <AppContext.Provider value={user}>
+      <div>
+        <Posts />
+      </div>
+    </AppContext.Provider>
+  );
+};
+
+// 1. PostsContext 생성
+const PostsContext = createContext();
+
+const Posts = () => {
+  const posts = [
+    {
+      title: "useContext란 무엇인가",
+      content: "상태관리 hooks인 useContext는 ...",
+    },
+  ];
+
+  return (
+    <PostsContext.Provider value={posts}>
+      <Children />
+    </PostsContext.Provider>
+  );
+};
+
+// 2. user와 posts를 가져와 화면에 보여주기
+const Children = () => {
+  const user = useContext(AppContext);
+  const posts = useContext(PostsContext);
+
+  let label = "user";
+  if (user.isAdmin) {
+    label = "admin";
+  }
+
+  return (
+    <div>
+      <div>{label}</div>
+      <div>{user.name}</div>
+      <div>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <div>{post.title}</div>
+            <div>{post.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
-export default App;
+};
