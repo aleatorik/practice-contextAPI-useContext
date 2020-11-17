8262f157aa6ffc7a121c7146a84325b0e18c3959
daseulsong <vates0328@gmail.com>
2020-11-17 20:36:09 +0900

    add: Add another usage of useContext

--- src/App.js
+++ src/App.js
@@ -1,3 +1,41 @@
+// ************************************************************************************************
+// ************************************************************************************************
+// EXAMPLE 1
+// ************************************************************************************************
+// ************************************************************************************************
+
+import React, { useContext, createContext } from "react";
+
+const TestContext = createContext();
+
+const TestContext2 = createContext();
+
+const UseContextExample = () => {
+  const hello = useContext(TestContext);
+  const world = useContext(TestContext2);
+  return <div>{hello + " " + world}</div>;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <TestContext.Provider value="hello">
+        <TestContext2.Provider value="world!">
+          <UseContextExample />
+        </TestContext2.Provider>
+      </TestContext.Provider>
+    </div>
+  );
+}
+
+export default App;
+
+//************************************************************************************************
+//************************************************************************************************
+// EXAMPLE 2
+//************************************************************************************************
+//************************************************************************************************
+
 // import React, { createContext, useContext } from "react";
 
 // // 0. AppContext 생성
@@ -64,75 +102,87 @@
 
 // export default App;
 
+//************************************************************************************************
+//************************************************************************************************
+// EXAMPLE 3
+//************************************************************************************************
+//************************************************************************************************
+
 // 아래는 Provider와 Consumer 함수를 활용한 Context API. 위의 hooks와 비교를 위해 어떤 것이 더 사용하기 편한지 비교하기 위함.
 
-import React, { createContext } from "react";
+// import React, { createContext } from "react";
 
-// 0. AppContext 생성
-const AppContext = createContext();
+// // 0. AppContext 생성
+// const AppContext = createContext();
 
-const App = () => {
-  const user = {
-    name: "daseul",
-    isAdmin: true,
-  };
+// const App = () => {
+//   const user = {
+//     name: "daseul",
+//     isAdmin: true,
+//   };
 
-  return (
-    <AppContext.Provider value={user}>
-      <div>
-        <Posts />
-      </div>
-    </AppContext.Provider>
-  );
-};
+//   return (
+//     <AppContext.Provider value={user}>
+//       <div>
+//         <Posts />
+//       </div>
+//     </AppContext.Provider>
+//   );
+// };
 
-// 1. PostsContext 생성
-const PostsContext = createContext();
+// // 1. PostsContext 생성
+// const PostsContext = createContext();
 
-const Posts = () => {
-  const posts = [
-    {
-      title: "useContext란 무엇인가",
-      content: "상태관리 hooks인 useContext는 ...",
-    },
-  ];
+// const Posts = () => {
+//   const posts = [
+//     {
+//       title: "useContext란 무엇인가",
+//       content: "상태관리 hooks인 useContext는 ...",
+//     },
+//   ];
 
-  return (
-    <PostsContext.Provider value={posts}>
-      <Children />
-    </PostsContext.Provider>
-  );
-};
+//   return (
+//     <PostsContext.Provider value={posts}>
+//       <Children />
+//     </PostsContext.Provider>
+//   );
+// };
 
-// 2. user와 posts를 가져와 화면에 보여주기
-const Children = () => (
-  <AppContext.Consumer>
-    {(user) => (
-      <PostsContext.Consumer>
-        {(posts) => {
-          let label = "user";
-          if (user.isAdmin) {
-            label = "admin";
-          }
+// // 2. user와 posts를 가져와 화면에 보여주기
+// const Children = () => (
+//   <AppContext.Consumer>
+//     {(user) => (
+//       <PostsContext.Consumer>
+//         {(posts) => {
+//           let label = "user";
+//           if (user.isAdmin) {
+//             label = "admin";
+//           }
 
-          return (
-            <div>
-              <div>{label}</div>
-              <div>{user.name}</div>
-              <div>
-                {posts.map((post, index) => (
-                  <div key={index}>
-                    <div>{post.title}</div>
-                    <div>{post.content}</div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        }}
-      </PostsContext.Consumer>
-    )}
-  </AppContext.Consumer>
-);
+//           return (
+//             <div>
+//               <div>{label}</div>
+//               <div>{user.name}</div>
+//               <div>
+//                 {posts.map((post, index) => (
+//                   <div key={index}>
+//                     <div>{post.title}</div>
+//                     <div>{post.content}</div>
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//           );
+//         }}
+//       </PostsContext.Consumer>
+//     )}
+//   </AppContext.Consumer>
+// );
 
-export default App;
+// export default App;
+
+//************************************************************************************************
+//************************************************************************************************
+// EXAMPLE 4
+//************************************************************************************************
+//************************************************************************************************
